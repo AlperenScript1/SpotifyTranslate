b72b2ccf79f9593d692e6a78e5a045464a1c4482
Alperen <alperensenscript@gmail.com>
2025-11-17 00:40:54 +0300

    Add files via upload

---
+++ SourceCode/SpotifyTranslate.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+import threading
+import time
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy
+#! Selenium
+from selenium import webdriver  
+import chromedriver_autoinstaller 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from time import sleep, time
+import random
+import re
+
+chromedriver_autoinstaller;
+options = Options()
+
+#! Kullanılmıyor
+def clean(text):
+    text = text.lower()
+    text = re.sub(r"[^a-z0-9]+", "", text)  # boşluk ve özel karakterleri tire yap
+    return text.strip("-")  # baş/sondaki tireleri sil
+options.add_argument("--window-size=1920,800") #? Sayfa görünür olmasa bile çözünürlüğü ayarlar.
+
+#! Selenium
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
+#options.add_argument("--headless") 
+driver = webdriver.Chrome(options=options); 
+url = "https://azlyrics.com" #! Sözlerin sağlandığı platform azlyrics
+driver.get(url)
+
+#? Spotify API  "client_id" & "client_secret"
+spotify = spotipy.Spotify(
+    auth_manager=SpotifyOAuth( #! Tokenler kontrol edilecek [SSL error code]
+        client_id="5d9b8645384941bb872cdc7cd4cc40a4", #! [17636:31252:1116/232551.244:ERROR:net\socket\ssl_client_socket_impl.cc:916] handshake failed; returned -1, SSL error code 1, net_error -101
+        client_secret="017f56dba2444610b24af44b430c54a3", 
+        redirect_uri="http://127.0.0.1:8000/callback",
+        scope="user-read-playback-state"
+    )
+)
+
+def poller():
+    while True:
+        try:
+            current = spotify.current_playback()
+            if current and current["is_playing"]:
+                firstTrack = current["item"]["name"]
+                lastTrack = firstTrack
+                artist = current["item"]["artists"][0]["name"]
+                track_Label1.config(text=f"{artist} - {lastTrack}")
+                print(artist +" "+ lastTrack) 
+                if artist and lastTrack != None:
+                        print("===========================================================")
+                        sleep(1)
+                        driver.get("https://www.azlyrics.com/lyrics/" + clean(artist) + "/" +clean(lastTrack) + ".html" ) #! https://www.azlyrics.com/lyrics/cigarettesaftersex/silversable.html
+                        sleep(2)
+                        driver.execute_script("window.scrollBy(0, 1200)") #! 1200 pixel downPage
+                        sleep(5) #! yüklendiği zaman kullanılacak
+                        lyrics = driver.find_element(By.XPATH,"//body/div[@class='container main-page']/div[@class='row']/div[@class='col-xs-12 col-lg-8 text-center']/div[5]").text #! lyrics
+                        print(lyrics);
+                        if(lyrics != None):
+                            print("Şarkı sözleri alındı !");
+                        else:
+                            print("Şarkı sözleri alınamadı..");
+                else:
+                    print("Spotify bağlantısı kurulamadı..");
+            else:
+                track_Label1.config(text="Çalmıyor")
+        except Exception as e:
+            error_Label1.config(text=f"Hata: {e}")
+        
+        sleep(2)
+            
+#! UI
+root = tk.Tk()
+root.title("Spotify Takip") 
+root.geometry("600x500")
+root.configure(bg="black")
+
+error_Label1 = tk.Label(root, text="Console", font=("Arial", 15), fg="red")
+error_Label1.pack(pady=20)
+
+track_Label1 = tk.Label(root, text="Bekleniyor...", font=("Arial", 15))
+track_Label1.pack(pady=20)
+
+#? threading olayı UI ve backend ayrı çalışması için.
+threading.Thread(target=poller, daemon=True).start()
+root.mainloop()
